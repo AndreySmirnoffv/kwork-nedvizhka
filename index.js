ffa21b08251c29e7800d318e2c0821d466cf7726
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2024-04-22 23:16:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+require('dotenv').config({path: './assets/modules/.env'})
 const TelegramApi = require('node-telegram-bot-api')
 const bot = new TelegramApi(process.env.devStatus ? process.env.TEST_TOKEN : process.env.DEFAULT_TOKEN, {polling: true})
 const fs = require('fs')
 const { startKeyboard, locationKeyboard, placeKeyboard, typekeyboard } = require('./assets/keyboard/keyboard')
-const commands = JSON.parse(fs.readFileSync('./assets/commands/commands.json'))
+const commands = JSON.parse(fs.readFileSync('./assets/db/commands/commands.json'))
 const db = require('./assets/db/db.json')
 
 bot.setMyCommands(commands)
 
 bot.on('message', async msg => {
-    let user = db.filter(user => user.username === msg.from.username)
+    let user = db.find(user => user.username === msg.from.username)
     if (msg.text === '/start'){
         if(!user){
             db.push({
@@ -17,15 +18,21 @@ bot.on('message', async msg => {
                 place: "",
                 type: ""
             })
+            fs.writeFileSync('./assets/db/db.json', JSON.stringify(db, null, '\t'))
         }
         await bot.sendMessage(msg.chat.id, "Здравствуйте, я Бот по поиску недвижимости на о. Бали", startKeyboard)
     }
 })
 
 bot.on('callback_query', async msg => {
+    const usernameToFind = msg.message.from.username
+    let user = db.find(user => user.username === msg.message.from.username)
+
     switch(msg.data){
         case "location":
             await bot.sendMessage(msg.message.chat.id, "Выбери локаицю: ", locationKeyboard)
+            user.location = msg.data
+            fs.writeFileSync('./assets/db/db.json', JSON.stringify(db, null, '\t'))
             break
         case "bukit" || "sanur" || "kuta" || "ubud" || "islands":
             await bot.sendMessage(msg.message.chat.id, "Выбери место:", placeKeyboard)
