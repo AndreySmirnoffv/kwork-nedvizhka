4b2e05480f46bf3e127b8931ed7eb32bbe9df999
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2024-04-23 10:18:06 +0200

    Add files via upload

--- assets/keyboard/keyboard.js
+++ assets/keyboard/keyboard.js
@@ -12,6 +12,7 @@ module.exports = {
         reply_markup: JSON.stringify({
             inline_keyboard: [
                 [{text: "Букит", callback_data: "bukit"}, {text: "Санур", callback_data: "sanur"}, {text: "Кута", callback_data: "kuta"}, {text: "Убуд", callback_data: "ubud"}, {text: "Острова", callback_data: "islands"}],
+                [{text: "Все", callback_data: "all"}],
                 [{text: "Назад", callback_data: "back"}]
             ]
         })
@@ -20,6 +21,7 @@ module.exports = {
         reply_markup: JSON.stringify({
             inline_keyboard: [
                 [{text: "Листхолд", callback_data: "listhold"}, {text: "Фрихолд", callback_data: "frihold"}],
+                [{text: "Все", callback_data: "alltwo"}],
                 [{text: "Назад", callback_data: "backtwo"}],
             ]
         })
@@ -28,6 +30,7 @@ module.exports = {
         reply_markup: JSON.stringify({
             inline_keyboard: [
                 [{text: "Гостинечный комплекс", callback_data: "hotel"}, {text: "Вилла", callback_data: "villa"}, {text: "Аппартаменты", callback_data: "appartments"}, {text: "Земля", callback_data: "ground"}],
+                [{text: "Все", callback_data: "allthree"}]
                 [{text: "Назад", callback_data: "backthree"}]
             ]
         })
--- index.js
+++ index.js
@@ -25,17 +25,17 @@ bot.on('message', async msg => {
 })
 
 bot.on('callback_query', async msg => {
-    const usernameToFind = msg.message.from.username
-    let user = db.find(user => user.username === msg.message.from.username)
+    let user = db.find(user => user.username === msg.from.username)
 
     switch(msg.data){
         case "location":
             await bot.sendMessage(msg.message.chat.id, "Выбери локаицю: ", locationKeyboard)
-            user.location = msg.data
-            fs.writeFileSync('./assets/db/db.json', JSON.stringify(db, null, '\t'))
             break
         case "bukit" || "sanur" || "kuta" || "ubud" || "islands":
             await bot.sendMessage(msg.message.chat.id, "Выбери место:", placeKeyboard)
+            user.location = "bukit"
+            fs.writeFileSync('./assets/db/db.json', JSON.stringify(db, null, '\t'))
+            console.log(msg.message.text)
             break
         case "listhold" || "frihold":
             await bot.sendMessage(msg.message.chat.id, "Выбери недвижимость: ", typekeyboard)
@@ -48,6 +48,12 @@ bot.on('callback_query', async msg => {
             break
         case "backthree":
             await bot.sendMessage(msg.message.chat.id, "Выбери локаицю: ", locationKeyboard)
+            break
+        case 'all':
+            break
+        case 'alltwo':
+            break
+        case "allthree":
             break
     }
 })
